acecb753ae09f2f523bf4908306833e747c286bf
unknown <M106-24@M106-24.iscm.char>
2019-03-18 10:11:18 +0300

    m

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 main()
 {
 float a, b;
 printf ("Введите значения a и b" \n);
 scanf ("%f%f", &a, &b);
-printf ("Сумма: %f\n, (a+b));
+printf ("Разность: %f\n, (a-b));
 return 0;
 }
